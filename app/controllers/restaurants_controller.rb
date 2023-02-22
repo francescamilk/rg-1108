27bfaa56ff526d1fc807771c1de2d07189592911
Francesca Santoriello <f.santoriello@hotmail.com>
2023-02-22 09:43:58 +0100

    refactor

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,7 +6,8 @@ class RestaurantsController < ApplicationController
     end
 
     def top
-        @restaurants = Restaurant.all.select { |restaurant| restaurant.rating == 5 }
+        # @restaurants = Restaurant.all.select { |restaurant| restaurant.rating == 5 }
+        @restaurants = Restaurant.where(rating: 5)
     end
 
     def show; end
